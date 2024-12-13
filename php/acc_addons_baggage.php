af39f89ab1475d352e8c78298ed143894c4e41a6
Jesreal D. Lustre <115926421+JESREAL1JDL7LUSTRE@users.noreply.github.com>
2024-12-13 15:09:45 +0800

    s

--- php/acc_addons_baggage.php
+++ php/acc_addons_baggage.php
@@ -61,7 +61,7 @@ $selectedAddons = isset($_SESSION['selected_addons']) ? $_SESSION['selected_addo
                 <td><?php echo htmlspecialchars($row['Baggage_Weight']); ?></td>
                 <td><?php echo htmlspecialchars($row['Price']); ?></td>
                 <td>
-                <form method="POST" action="acc_addons.php">
+                <form method="POST">
                     <input type="hidden" name="addon_id" value="<?php echo $row['Baggage_ID']; ?>">
                     <input type="hidden" name="addon_name" value="<?php echo $row['Baggage_Weight']; ?>">
                     <input type="hidden" name="addon_price" value="<?php echo $row['Price']; ?>">
